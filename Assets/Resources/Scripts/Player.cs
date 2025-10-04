0278062eae175c6c53e2d04d6c9e6c10cfd3a42b
MKD20590 <michelleandreas123@gmail.com>
2025-10-04 18:23:26 +0700

    movement fixed & lighting setup

--- Assets/Resources/Scripts/Player.cs
+++ Assets/Resources/Scripts/Player.cs
@@ -5,17 +5,19 @@ using static UnityEngine.InputSystem.InputAction;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float hunger = 100f;
+    [SerializeField] private float stamina = 50f;
     [SerializeField] private AudioSource footstepAudioSource;
     [SerializeField] private CinemachineBasicMultiChannelPerlin cinemachineNoise;
     Rigidbody rb;
     [SerializeField] private Vector3 movement = Vector3.zero;
-    Vector2 direction = Vector2.zero;
+    [SerializeField] private Vector2 direction = Vector2.zero;
     [SerializeField] private float normalSpeed = 5f;
     [SerializeField] private float sprintSpeed = 10f;
     [SerializeField] private float currentSpeed = 5f;
+    [SerializeField] private bool isSprinting = false;
     [SerializeField] private float jumpForce = 10f;
-    bool isJumped = false;
-    bool isSprinting = false;
+    [SerializeField] private bool isJumped = false;
     Camera cam;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private bool isGrounded = false;
@@ -34,15 +36,30 @@ public class Player : MonoBehaviour
     {
         //grouunding check
         Ray ray = new Ray(transform.position, -transform.up);
-        isGrounded = Physics.Raycast(ray, out hit, 1.2f, groundLayer);
+        isGrounded = Physics.Raycast(ray, out hit, 1.1f, groundLayer);
+
         //movement
+
         movement = (cam.transform.forward * direction.y) + (cam.transform.right * direction.x);
         movement.y = 0f;
         movement.Normalize();
-        currentSpeed = isSprinting ? sprintSpeed : normalSpeed;
-        if(isJumped && isGrounded)
+
+        if (isSprinting && stamina > 0)
+        {
+            stamina -= Time.deltaTime * 5f;
+        }
+        else if(!isSprinting && stamina < 50f)
+        {
+            stamina += Time.deltaTime * 3f;
+        }
+        currentSpeed = isSprinting && stamina > 0 ? sprintSpeed : normalSpeed;
+        if (isJumped && isGrounded)
         {
             rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
+        }
+        else
+        {
+            rb.linearVelocity = new Vector3(movement.x * currentSpeed, rb.linearVelocity.y, movement.z * currentSpeed);
         }
         CameraEffects();
     }
@@ -51,14 +68,12 @@ public class Player : MonoBehaviour
         if (direction != Vector2.zero)
         {
             camAmplitudeGain = isSprinting ? 1.5f : 1f;
-            camFrequencyGain = isSprinting ? 0.2f : 0.15f;
-            rb.linearVelocity = new Vector3(movement.x * currentSpeed, rb.linearVelocity.y, movement.z * currentSpeed);
+            camFrequencyGain = isSprinting ? 0.2f : 0.1f;
         }
         else
         {
             camAmplitudeGain = 0.2f;
             camFrequencyGain = 0.1f;
-            rb.linearVelocity = Vector3.MoveTowards(rb.linearVelocity, new Vector3(0, rb.linearVelocity.y, 0), Time.deltaTime * 10f);
         }
         cinemachineNoise.AmplitudeGain = Mathf.Lerp(cinemachineNoise.AmplitudeGain, camAmplitudeGain, Time.deltaTime * 10f);
         cinemachineNoise.FrequencyGain = Mathf.Lerp(cinemachineNoise.FrequencyGain, camFrequencyGain, Time.deltaTime * 10f);
@@ -102,5 +117,13 @@ public class Player : MonoBehaviour
                 Debug.Log("interact");
             }
         }
+    }
+    public float GetHunger()
+    {
+        return hunger;
+    }
+    public void AddHunger(float value)
+    {
+        hunger += value;
     }
 }
