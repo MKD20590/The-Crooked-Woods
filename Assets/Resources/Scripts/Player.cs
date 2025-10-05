092c33caf0764f52ca3f9969ac3659535a6d0922
MKD20590 <michelleandreas123@gmail.com>
2025-10-05 21:47:56 +0700

    walk sfx

--- Assets/Resources/Scripts/Player.cs
+++ Assets/Resources/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -7,7 +8,14 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float hunger = 100f;
     [SerializeField] private float stamina = 50f;
+    [SerializeField] private AudioClip footstepClip_grass;
+    [SerializeField] private AudioClip footstepClip_path;
     [SerializeField] private AudioSource footstepAudioSource;
+    [SerializeField] private AudioSource jumpAudioSource;
+    [Range(0f, 1f)]
+    [SerializeField] private float footstepPitchInterval;
+    [SerializeField] private float footstepInterval_walk = 0.5f;
+    [SerializeField] private float footstepInterval_run = 0.2f;
     [SerializeField] private CinemachineBasicMultiChannelPerlin cinemachineNoise;
     Rigidbody rb;
     [SerializeField] private Vector3 movement = Vector3.zero;
@@ -34,12 +42,11 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //grouunding check
+        //grounding check
         Ray ray = new Ray(transform.position, -transform.up);
-        isGrounded = Physics.Raycast(ray, out hit, 1.1f, groundLayer);
+        isGrounded = Physics.Raycast(ray, out hit, 1.05f, groundLayer);
 
         //movement
-
         movement = (cam.transform.forward * direction.y) + (cam.transform.right * direction.x);
         movement.y = 0f;
         movement.Normalize();
@@ -56,6 +63,10 @@ public class Player : MonoBehaviour
         if (isJumped && isGrounded)
         {
             rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
+            //play 1 time footstep sound when jump
+            jumpAudioSource.Stop();
+            jumpAudioSource.transform.position = new Vector3(transform.position.x, transform.position.y - 0.7f, transform.position.z);
+            jumpAudioSource.Play();
         }
         else
         {
@@ -85,6 +96,15 @@ public class Player : MonoBehaviour
             if (ctx.phase == InputActionPhase.Performed || ctx.phase == InputActionPhase.Canceled)
             {
                 direction = ctx.ReadValue<Vector2>();
+                if (ctx.phase == InputActionPhase.Performed)
+                {
+                    StopAllCoroutines();
+                    StartCoroutine(FootstepSFX());
+                }
+                else
+                {
+                    StopAllCoroutines();
+                }
             }
         }
     }
@@ -96,6 +116,8 @@ public class Player : MonoBehaviour
             {
                 isSprinting = ctx.ReadValueAsButton();
             }
+            StopAllCoroutines();
+            StartCoroutine(FootstepSFX());
         }
     }
     public void Jump(CallbackContext ctx)
@@ -106,6 +128,52 @@ public class Player : MonoBehaviour
             {
                 isJumped = ctx.ReadValueAsButton();
             }
+        }
+    }
+    IEnumerator FootstepSFX()
+    {
+        while(true)
+        {
+            if(isGrounded)
+            {
+                //play footstep sound
+                footstepAudioSource.transform.position = new Vector3(transform.position.x, transform.position.y-0.7f, transform.position.z);
+                footstepAudioSource.pitch = Random.Range(1f - footstepPitchInterval, 1f + footstepPitchInterval);
+                footstepAudioSource.Play();
+            }
+            else
+            {
+                footstepAudioSource.Stop();
+            }
+            float delay = 0;
+            if(isGrounded && isSprinting && stamina <= 0 || isGrounded && !isSprinting)
+            {
+                delay = footstepInterval_walk;
+            }
+            else if(isGrounded && isSprinting && stamina > 0)
+            {
+                delay = footstepInterval_run;
+            }
+            else if (!isGrounded)
+            {
+                delay = 0;
+            }
+            yield return new WaitForSeconds(delay);                
+        }
+    }
+    public void ChangeFootstepSFX(string surface)
+    {
+        switch (surface)
+        {
+            case "Grass":
+                footstepAudioSource.clip = footstepClip_grass;
+                break;
+            case "Path":
+                footstepAudioSource.clip = footstepClip_path;
+                break;
+            default:
+                footstepAudioSource.clip = footstepClip_grass;
+                break;
         }
     }
     public void Interact(CallbackContext ctx)
