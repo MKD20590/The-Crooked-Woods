1e433f855eeb8e57eae5e4671f6d2374f81bd510
NasiMentega <rafaelww05@gmail.com>
2025-10-14 18:02:34 +0700

    Main Menu Set Up
    + Adding Main Menu

---
+++ Assets/Resources/Scripts/SceneHandler.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        Time.timeScale = 1f; // Ensure time scale is reset before loading a new scene
+        SceneManager.LoadScene(sceneName);
+    }
+}
